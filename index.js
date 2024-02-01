17ceb5dcc50af03eeab92adaee03d06010a41a03
javascripterzzeerrr <stroganov_n777@mail.ru>
2024-02-01 22:30:08 +0300

    try fix work netlify

--- index.js
+++ index.js
@@ -10,5 +10,8 @@ const app = express();
 app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
 app.use('/.netlify/functions/api', router);
 
+router.get('/test', (req, res) => {
+    res.send('App is running...');
+});
 
 app.listen(PORT, () => console.log(`Express server listening on port ${PORT}}`));
