f3a7e5f0a0dd6396a9090523d769ef64779a92d9
javascripterzzeerrr <stroganov_n777@mail.ru>
2024-02-01 22:02:37 +0300

    add netlify

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path'); // модуль для парсинга пути
 const router = require('./routes/router');
-import serverless from "serverless-http";
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,7 +9,7 @@ const app = express();
 
 //app.use(favicon(options.favicon));//Загружаем нашу иконку
 app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
-app.use('/api', router);
+app.use('/.netlify/functions/api', router);
 
 export const handler = serverless(api);
 
