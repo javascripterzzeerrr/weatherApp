f838911b1db8039a116516ba45cb0fcebae869ee
javascripterzzeerrr <stroganov_n777@mail.ru>
2024-02-01 22:37:56 +0300

    try fix work netlify4 - add serverless

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const router = require('./routes/router');
+const serverless = require('serverless-http');
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -14,5 +15,6 @@ router.get('/test', (req, res) => {
 });
 
 app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless;
 
-app.listen(PORT, () => console.log(`Express server listening on port ${PORT}}`));
+// app.listen(PORT, () => console.log(`Express server listening on port ${PORT}}`));
