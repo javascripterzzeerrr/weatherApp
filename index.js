605e87dff7674707217aff219271ccc8edcd98b7
javascripterzzeerrr <stroganov_n777@mail.ru>
2024-02-01 22:26:58 +0300

    try fix work netlify

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path'); // модуль для парсинга пути
 const router = require('./routes/router');
 
 const PORT = process.env.PORT || 5000;
@@ -10,8 +9,6 @@ const app = express();
 //app.use(favicon(options.favicon));//Загружаем нашу иконку
 app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
 app.use('/.netlify/functions/api', router);
-
-export const handler = serverless(api);
 
 
 app.listen(PORT, () => console.log(`Express server listening on port ${PORT}}`));
