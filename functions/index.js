bbcfe81f079d30dc18bd664bec4546a20a2d1e16
javascripterzzeerrr <stroganov_n777@mail.ru>
2024-02-02 19:32:09 +0300

    update controller post

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,24 @@
-require('dotenv').config();
 const express = require('express');
 const router = require('./routes/router');
 const serverless = require('serverless-http');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 // const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 //app.use(favicon(options.favicon));//Загружаем нашу иконку
-app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
+//app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
 
 router.get('/test', (req, res) => {
     res.send('App is running...');
 });
 
 app.use('/.netlify/functions/api', router);
-module.exports.handler = serverless;
+
+app.use(errorHandler);
+
+
+module.exports.handler = serverless(app);
 
 // app.listen(PORT, () => console.log(`Express server listening on port ${PORT}}`));
